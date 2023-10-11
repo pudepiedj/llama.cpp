6e453f6546fb279f3c6c51f5edfd6add6b95db9b
pudepiedj <pudepiedj@gmail.com>
2023-10-11 17:41:36 +0100

    cfind with one regex

--- examples/cmap-example/cfindimplementedargs.cpp
+++ examples/cmap-example/cfindimplementedargs.cpp
@@ -10,29 +10,26 @@
 #include <unordered_map>
 #include <algorithm>
 
-// The function to update the source file
-void update_file(const std::string& file_from, const std::string& file_to = "c_help_list.txt") {
-    std::ifstream in_file(file_from);
-    std::ofstream out_file(file_to);
-
-    if (in_file && out_file) {
-        std::string line;
-        // std::regex pattern(R"printf\("\s(.*?)\);");
-        // std::regex pattern(R"printf\("\s(.*?)\);");
-        std::regex pattern("printf\\(\"\\s(.*?)\\);");
-
-        while (std::getline(in_file, line)) {
-            std::smatch matches;
-            if (std::regex_search(line, matches, pattern)) {
-                out_file << matches[1] << '\n';
-            }
+// function to replace hyphens with underscores
+std::string replace_hyphens_with_underscores(const std::string& input) {
+    std::string result = input;
+    for (size_t i = 0; i < result.length(); i++) {
+        if (result[i] == '-') {
+            result[i] = '_';
         }
-
-        in_file.close();
-        out_file.close();
-    } else {
-        std::cerr << "Failed to open files." << std::endl;
     }
+    return result;
+}
+
+// function to replace underscores with hyphens
+std::string replace_underscores_with_hyphens(const std::string& input) {
+    std::string result = input;
+    for (size_t i = 0; i < result.length(); i++) {
+        if (result[i] == '_') {
+            result[i] = '-';
+        }
+    }
+    return result;
 }
 
 // The function to replace dashes with underscores in a file
@@ -45,8 +42,7 @@ void replace_dashes_with_underscores(const std::string& filename) {
         buffer << in_file.rdbuf();
         content = buffer.str();
 
-        std::regex pattern(R"(\w)-(\w)");
-        replacedContent = std::regex_replace(content, pattern, "$1_$2");
+        replace_hyphens_with_underscores(content);
 
         in_file.close();
 
@@ -62,6 +58,64 @@ void replace_dashes_with_underscores(const std::string& filename) {
     }
 }
 
+// the functions to capture everything between quotes in print statements)
+bool is_in_print_statement(const std::string& line) {
+    return line.find("printf(") != std::string::npos;
+}
+
+std::string extract_print_content(const std::string& line) {
+    size_t start = line.find("\"") + 1;
+    size_t end = line.find_last_of("\"");
+    if (start != std::string::npos && end != std::string::npos && end > start) {
+        return line.substr(start, end - start);
+    }
+    return "";
+}
+
+// The function to update the source file
+void update_file(const std::string& file_from, const std::string& file_to = "c_help_list.txt") {
+    std::ifstream in_file(file_from);
+    std::ofstream out_file(file_to);
+
+    if (in_file && out_file) {
+        std::string line;
+
+        while (std::getline(in_file, line)) {
+            if (is_in_print_statement(line)) {
+                std::string content = extract_print_content(line);
+                printf("Content: %s",content.c_str());
+            }
+        }
+        in_file.close();
+        out_file.close();
+    } else {
+        std::cerr << "Failed to open files." << std::endl;
+    }
+}
+
+// match the params strings up to the stipulated next character
+std::string capture_after_params(const std::string& content) {
+    std::size_t startPos = std::string::npos;
+    std::string delimiter = " \n})(,>;";
+
+    if (content.find("params->") != std::string::npos) {
+        startPos = content.find("params->") + 8;  // Adjust for the length of "params->"
+    } else if(content.find("params.") != std::string::npos) {
+        startPos = content.find("params.") + 7;   // Adjust for the length of "params."
+    }
+
+    if (startPos != std::string::npos) {
+        std::size_t endPos = content.find_first_of(delimiter, startPos);
+        if (endPos != std::string::npos) {
+            return content.substr(startPos, endPos - startPos);
+        } else {
+            return content.substr(startPos);  // Capture until end of string
+        }
+    }
+
+    return "";  // Pattern not found
+}
+
 // The function to find arguments in a directory
 std::unordered_map<std::string, std::unordered_set<std::string>> find_arguments(const std::string& directory) {
     std::unordered_map<std::string, std::unordered_set<std::string>> arguments;
@@ -69,29 +123,38 @@ std::unordered_map<std::string, std::unordered_set<std::string>> find_arguments(
     for (const auto& entry : std::filesystem::recursive_directory_iterator(directory)) {
         if (entry.path().extension() == ".cpp") {
             std::ifstream in_file(entry.path());
-            std::string content, line;
 
             if (in_file) {
                 std::stringstream buffer;
                 buffer << in_file.rdbuf();
-                content = buffer.str();
+                std::string content = buffer.str();
 
-                std::regex pattern(R"((?:^|\b)params[->\.]([a-zA-Z_0-9]*)(?=[\). <,;}]|\Z))");
                 std::unordered_set<std::string> matches;
 
-                for (std::sregex_iterator it(content.begin(), content.end(), pattern), end; it != end; ++it) {
-                    matches.insert((*it)[1].str());
+                std::size_t pos = 0;
+                while (pos != std::string::npos) {
+                    pos = content.find_first_of("params.", pos);
+                    if (pos != std::string::npos) {
+                        matches.insert(capture_after_params(content.substr(pos)));
+                        pos++;
+                    }
+                    pos = content.find_first_of("params->", pos);
+                    if (pos != std::string::npos) {
+                        matches.insert(capture_after_params(content.substr(pos)));
+                        pos++;
+                    }
                 }
 
-                arguments[entry.path().string()] = std::move(matches);
+                for (const auto& match : matches) {
+                    std::cout << match << std::endl;
+                }
 
-                in_file.close();
-            } else {
-                std::cerr << "Failed to open file: " << entry.path().string() << std::endl;
-            }
-        }
-    }
-
+                    in_file.close();
+        } else {
+                        std::cerr << "Failed to open file: " << entry.path().string() << std::endl;
+                        }
+                    }
+                }
     return arguments;
 }
 
@@ -129,12 +192,6 @@ std::string concatenate(const std::vector<std::string>& v) {
     return concatenated_element;
 }
 
-// The function to replace underscores with dashes
-std::string replace_underscores_with_dashes(const std::string& parameter) {
-    std::regex pattern(R"(\w)_(\w)");
-    return std::regex_replace(parameter, pattern, "$1-$2");
-}
-
 // The function to find parameters in the help file
 void find_parameters(const std::string& file, const std::unordered_map<std::string, std::unordered_set<std::string>>& result) {
     std::ifstream help_file(file);
@@ -151,14 +208,13 @@ void find_parameters(const std::string& file, const std::unordered_map<std::stri
         for (const auto& [filename, arguments] : result) {
             std::unordered_set<std::string> substituted_arguments;
             for (const auto& argument : arguments) {
-                substituted_arguments.insert(replace_underscores_with_dashes(argument));
+                substituted_arguments.insert(replace_underscores_with_hyphens(argument));
             }
 
             std::vector<std::string> matched_lines;
             for (const auto& line : lines) {
                 for (const auto& argument : substituted_arguments) {
-                    std::regex pattern(R"(--)" + argument + R"(\s)|(?:params\.)" + argument +
-                                       R"(\b(?=[\s.,\.\(\);]|\.+\w)))");
+                    std::regex pattern(R"(--)" + argument + R"(\s)|(?:params\.)" + argument + R"(\b(?=[\s\.,;]|\.+\w)))");
 
                     if (std::regex_search(line, pattern)) {
                         matched_lines.push_back(line);
