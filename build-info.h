795cd5a8976907d586ff0d18d8786d66f5d825c4
pudepiedj <pudepiedj@gmail.com>
2023-11-20 12:54:46 +0000

    Mostly textual changes in prompt files

---
+++ build-info.h
@@ -0,0 +1,9 @@
+#ifndef BUILD_INFO_H
+#define BUILD_INFO_H
+
+#define BUILD_NUMBER 1413
+#define BUILD_COMMIT "cf81e33"
+#define BUILD_COMPILER "Apple clang version 15.0.0 (clang-1500.0.40.1)"
+#define BUILD_TARGET "arm64-apple-darwin23.0.0"
+
+#endif // BUILD_INFO_H
