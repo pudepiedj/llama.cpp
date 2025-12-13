d79025a72807af815e43c5d8a005ab9851c2dadf
pudepiedj <pudepiedj@gmail.com>
2025-12-13 11:16:27 +0000

    Major llama update

---
+++ common/build-info.cpp
@@ -0,0 +1,4 @@
+int LLAMA_BUILD_NUMBER = 5607;
+char const *LLAMA_COMMIT = "95987fbb";
+char const *LLAMA_COMPILER = "Apple clang version 17.0.0 (clang-1700.0.13.5)";
+char const *LLAMA_BUILD_TARGET = "arm64-apple-darwin24.5.0";
